00ecf41c4b436cf5a48eefd9d50a9f77ffd24c44
Dmitriy <dim116@inbox.ru>
2023-05-15 16:21:35 +0300

    changed logic

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,8 +3,8 @@ fun main(args: Array<String>) {
     val constClient = true
     val afterFirstDiscount = (if (amount>=0 && amount<=1000) amount
     else if(amount>1000 && amount<=10000) (amount-100)
-    else amount/1.05).toInt()
-    val afterSecondDiscount = (if(constClient) afterFirstDiscount/1.01
+    else amount*0.95).toInt()
+    val afterSecondDiscount = (if(constClient) afterFirstDiscount*0.99
     else afterFirstDiscount).toInt()
 println("Итоговая сумма: $afterSecondDiscount руб")
 }
